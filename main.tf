88ad48bf59dccd826e906b5b752e649962c994ca
Richard Chmielek <github@chmielek.com>
2022-06-23 17:47:05 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "random_string" "environment" {
+  length  = 8
+  special = false
+  numeric = false
+  upper = false
+}
+
+locals {
+  environment = resource.random_string.environment.result
+}
+
+resource "azurecaf_name" "resource_group" {
+  name          = var.application_name
+  resource_type = "azurerm_resource_group"
+  suffixes      = [local.environment]
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = azurecaf_name.resource_group.result
+  location = var.location
+
+  tags = {
+    "terraform"        = "true"
+    "environment"      = local.environment
+    "application-name" = var.application_name
+    "nubesgen-version" = "0.11.5"
+  }
+}
+
+module "application" {
+  source           = "./modules/app-service"
+  resource_group   = azurerm_resource_group.main.name
+  application_name = var.application_name
+  environment      = local.environment
+  location         = var.location
+
+  azure_application_insights_instrumentation_key = module.application-insights.azure_application_insights_instrumentation_key
+}
+
+module "application-insights" {
+  source           = "./modules/application-insights"
+  resource_group   = azurerm_resource_group.main.name
+  application_name = var.application_name
+  environment      = local.environment
+  location         = var.location
+}
